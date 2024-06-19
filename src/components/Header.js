c07c1eb4367a9b9cdf6b0be2c6ac899d6f65d342
Farideh <heidarian89@gmail.com>
2024-06-19 06:07:06 +0330

    add style to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,27 @@ export default function Header() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <div>
-      <Link to="/">NatureGram</Link>
+    <div className="flex items-center justify-between border-b border-borderGray">
+      <Link to="/" className="hover:bg-hoverColor hover:text-white p-4">
+        NatureGram
+      </Link>
       <input
         type="text"
         placeholder="Search here..."
         onChange={(e) => setSearchParams({ title: e.target.value })}
+        className="p-2 border border-borderGray w-1/4"
       />
-      <Link to="/about">About</Link>
-      <Link to="/createNew">createNew</Link>
+      <div>
+        <Link
+          to="/createNew"
+          className="mr-3 hover:bg-hoverColor hover:text-white p-4"
+        >
+          createNew
+        </Link>
+        <Link to="/about" className="hover:bg-hoverColor hover:text-white p-4">
+          About
+        </Link>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: { colors: { borderGray: "#d3d3d3", hoverColor: "#069874" } },
   },
   plugins: [],
 };
-
